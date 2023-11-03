cda94e66c57e3e2dedd92ec7b40592d6143fe863
Mattia Ginoble <mattiaginoble@MBP-di-Mattia.homenet.telecomitalia.it>
2023-11-03 14:37:27 +0100

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Tilt from "react-parallax-tilt";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Tilt>
+        <div className="card">
+          <div id="card__header">
+            <div id="header__number">212 555 6342</div>
+            <div id="header__company">
+              <p id="company__title">Pierce &amp;Pierce</p>
+              <p id="company__subtitle">Mergers And Acquisitions</p>
+            </div>
+          </div>
+
+          <div id="card__body">
+            <p id="body__firstname">Mattia&nbsp;</p>
+            <p id="body__lastname">Ginoble</p>
+            <p id="body__title">UI/UX Designer</p>
+          </div>
+          <div id="card__footer">
+            <a
+              id="footer__text"
+              href="https://www.icloud.com/iclouddrive/0c8fPEgKi-rx-dB1wjKY3toHQ#CV_Mattia_Ginoble"
+            >
+              CV
+            </a>
+            <a
+              id="footer__text"
+              href="https://www.linkedin.com/in/mattia-ginoble-bb4890239/"
+            >
+              Linkedin
+            </a>
+            <a id="footer__text" href="https://github.com/mattiaginoble/">
+              GitHub
+            </a>
+            <a id="footer__text" href="https://dribbble.com/mattiaginble/">
+              Dribble
+            </a>
+            <a
+              id="footer__text"
+              href="https://discordapp.com/users/361177612623806467/"
+            >
+              Discord
+            </a>
+          </div>
+        </div>
+      </Tilt>
     </div>
   );
 }
